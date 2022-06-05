28bf33bcf4aed50365720f31392a1097ebaebdde
sajedul <98393471+sajedul43@users.noreply.github.com>
2022-06-05 15:47:38 +0900

    section crud

---
+++ app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Section;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subject = Section::all();
+        return response()->json($subject);
+    }
+
+   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'class_id' =>'required',
+            'section_name' =>'required'
+        ]);
+
+        $section = Section::create($request->all());
+        return response('section inserted successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $section = Section::where('id',$id)->first();
+        return response()->json($section);
+    }
+
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $section = Section::Find($id);
+        $section->update($request->all());
+        return response('subject updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $section = Section::Find($id);
+        $section->delete();
+        return response('section deleted successfully');
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ClassController;
 use App\Http\Controllers\Api\SubjectController;
+use App\Http\Controllers\Api\SectionController;
+
 
 
 
 //Route::apiResource('/class',[ClassController::class]);
 Route::apiResource('/class', ClassController::class);
 Route::apiResource('/subject', SubjectController::class);
+Route::apiResource('/section', SectionController::class);
+
 
